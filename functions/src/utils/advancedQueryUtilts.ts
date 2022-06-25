c851bfaed889766867435e619f8a14d6cfe5d909
Andrew Dimmer <contactandrewd@gmail.com>
2022-06-25 14:56:29 -0400

    feat(functions): Add Advanced Query Filter Uitls
    This allows playlist queries to be either a single tag, or a series of AND/OR statements.
    
    Working on #17.

---
+++ functions/src/utils/advancedQueryUtilts.ts
@@ -0,0 +1,114 @@
+import { readTagFirestore } from "../firebase/tagsDAO";
+import { intersectionLists, unionLists } from "./arrayUtils";
+import { logAndThrowError } from "./errorHandlingUtils";
+
+export const cleanFilterString = (filter: string) => {
+  const cleanFilter = filter.replace(/\s/g, "");
+  if (!cleanFilter) {
+    logAndThrowError("The filter cannot be empty.");
+  }
+  return cleanFilter;
+};
+
+export const validateFilterStringAndExtractTagIds = async (
+  filter: string
+): Promise<string[]> => {
+  // Single Tag Filter
+  if (filter.indexOf("TAG_") === 0) {
+    if (filter.replace(/[A-Za-z0-9-_]/g, "").length > 0) {
+      logAndThrowError(`The tag id=${filter} contains unexpected characters.`);
+    }
+
+    // Check tag exists and add to cache for future operations
+    await readTagFirestore(filter);
+    return [filter];
+  }
+
+  // AND or OR Filter
+  if (filter.indexOf("AND(") === 0 || filter.indexOf("OR(") === 0) {
+    if (filter.charAt(filter.length - 1) !== ")") {
+      logAndThrowError("Filter has unbalanced parentheses.");
+    }
+
+    // Remove the "AND" or "OR" from the filter
+    const filterStart = filter.indexOf("AND(") === 0 ? "AND(" : "OR(";
+    const filterSubstring = filter.substring(
+      filterStart.length,
+      filter.length - 1
+    );
+    const tagIds = await Promise.all(
+      filterSubstring.split(",").map((filterToken) => {
+        return validateFilterStringAndExtractTagIds(filterToken);
+      })
+    );
+    return tagIds.reduce((oldTagIds, newTagIds) => {
+      return unionLists(oldTagIds, newTagIds);
+    }, [] as string[]);
+  }
+
+  logAndThrowError(
+    "Invalid Filter: Start of filter is not a tag id, AND, or OR."
+  );
+  return []; // Should never happen, just needed for the type system
+};
+
+export const getMediaObjectIdsMatchingFilter = async (
+  filter: string
+): Promise<string[]> => {
+  if (filter.indexOf("TAG_") === 0) {
+    // Single Tag Filter
+    return (await readTagFirestore(filter)).mediaObjectIds;
+  } else {
+    // Compound Tag Filter
+    // Remove the "AND" or "OR" from the filter
+    const filterStart = filter.indexOf("AND(") === 0 ? "AND(" : "OR(";
+    const filterSubstring = filter.substring(
+      filterStart.length,
+      filter.length - 1
+    );
+    const mediaObjectIds = await Promise.all(
+      filterSubstring.split(",").map((filterToken) => {
+        return getMediaObjectIdsMatchingFilter(filterToken);
+      })
+    );
+
+    return mediaObjectIds.reduce(
+      (mediaOnjectMatchingFilter, newMediaObjectIds) => {
+        return filterStart === "AND("
+          ? unionLists(mediaOnjectMatchingFilter, newMediaObjectIds)
+          : intersectionLists(mediaOnjectMatchingFilter, newMediaObjectIds);
+      },
+      [] as string[]
+    );
+  }
+};
+
+export const checkIfMediaObejctMatchesFilter = (
+  filter: string,
+  mediaObjectTagIds: string[]
+): boolean => {
+  if (filter.indexOf("TAG_") === 0) {
+    // Single Tag Filter
+    return mediaObjectTagIds.includes(filter);
+  } else {
+    // Compound Tag Filter
+    // Remove the "AND" or "OR" from the filter
+    const filterStart = filter.indexOf("AND(") === 0 ? "AND(" : "OR(";
+    const filterSubstring = filter.substring(
+      filterStart.length,
+      filter.length - 1
+    );
+    const mediaObjectIds = filterSubstring.split(",").map((filterToken) => {
+      return checkIfMediaObejctMatchesFilter(filterToken, mediaObjectTagIds);
+    });
+
+    return mediaObjectIds.reduce(
+      (matchFilter, matchNewFilterToken) => {
+        return filterStart === "AND("
+          ? matchFilter && matchNewFilterToken
+          : matchFilter || matchNewFilterToken;
+      },
+      filterStart === "AND(" ? true : false
+    );
+  }
+};
--- functions/src/utils/arrayUtils.ts
+++ functions/src/utils/arrayUtils.ts
@@ -6,3 +6,27 @@ export const removeItemFromList = <T>(list: T[], itemToRemove: T) => {
     return outputList;
   }, [] as T[]);
 };
+
+export const unionLists = <T>(list1: T[], list2: T[]) => {
+  const outputListInitial = list1.concat([]);
+  return list2.reduce((outputList, list2item) => {
+    if (!outputList.includes(list2item)) {
+      outputList.push(list2item);
+    }
+    return outputList;
+  }, outputListInitial);
+};
+
+export const intersectionLists = <T>(list1: T[], list2: T[]) => {
+  // Short circuit empty lists
+  if (list1.length === 0 || list2.length === 0) {
+    return [];
+  }
+
+  return list1.reduce((outputList, list1item) => {
+    if (list2.includes(list1item)) {
+      outputList.push(list1item);
+    }
+    return outputList;
+  }, [] as T[]);
+};
